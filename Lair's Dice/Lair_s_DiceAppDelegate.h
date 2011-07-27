a7a4950cb29675cc2c24c9faa75d8b4fb2e6cb6f
Alex Turner <bluechill@d-109-214.eecs.umich.edu>
2011-07-27 14:40:21 -0400

    Alpha 3

--- Lair's Dice/Lair_s_DiceAppDelegate.h
+++ Lair's Dice/Lair_s_DiceAppDelegate.h
@@ -9,16 +9,10 @@
 #import <UIKit/UIKit.h>
 
 @interface Lair_s_DiceAppDelegate : NSObject <UIApplicationDelegate> {
-
+    UIWindow *window;
+    UIViewController *mainViewController;
 }
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
-
-@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
-@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
-@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
-- (void)saveContext;
-- (NSURL *)applicationDocumentsDirectory;
 
 @end
